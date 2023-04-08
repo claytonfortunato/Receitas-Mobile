9bdb9f5ac60af7427d1836b96114cebcf6b693e8
claytonfortunato <claytonreis10@gmail.com>
2023-04-07 21:37:31 -0300

    feat: pages and routes

---
+++ src/pages/detail/index.js
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export const Detail = () => {
+  return (
+    <View>
+      <Text>Página Detalhes da Receita</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#ddd",
+  },
+});
---
+++ src/pages/home/index.js
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Página Home</Text>
+      <Text>PAGINAAAAAA</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#ddd",
+  },
+});
