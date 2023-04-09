13fd901012981ba90f36e121b586e477b9d9fe87
claytonfortunato <claytonreis10@gmail.com>
2023-04-09 00:15:15 -0300

    feat: component foodlist finalizado

--- src/components/foodlist/index.js
+++ src/components/foodlist/index.js
@@ -1,13 +1,21 @@
 import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 
+import { LinearGradient } from "expo-linear-gradient";
+
 export function FoodList({ data }) {
   return (
     <TouchableOpacity style={styles.container}>
       <Image source={{ uri: data.cover }} style={styles.cover} />
       <View style={styles.info}>
         <Text style={styles.name}>{data.name}</Text>
-        <Text style={styles.description}>10 ingredientes | 60 min</Text>
+        <Text style={styles.description}>
+          {data.total_ingredients} ingredientes | {data.time} min
+        </Text>
       </View>
+      <LinearGradient
+        style={styles.gradient}
+        colors={["transparent", "rgba(0,0,0,0.70)", "rgba(0,0,0,0.95)"]}
+      />
     </TouchableOpacity>
   );
 }
@@ -26,13 +34,25 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 14,
     zIndex: 99,
+    marginBottom: 4,
   },
   name: {
-    fontSize: 18,
+    fontSize: 22,
     color: "#fff",
     fontWeight: "bold",
   },
   description: {
     color: "#fff",
+    fontSize: 16,
+  },
+  gradient: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: "55%",
+    borderRadius: 8,
+    zIndex: 1,
+    backgroundColor: "transparent",
   },
 });
